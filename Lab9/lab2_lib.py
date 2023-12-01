6e00ae040b4a32737c3830ecf61f6bd607aa6932
s316606 <s316606@studenti.polito.it>
2023-12-01 19:20:25 +0100

    Lab9

---
+++ Lab9/lab2_lib.py
@@ -0,0 +1,34 @@
+from abc import abstractmethod
+
+class AbstractProblem:
+    def __init__(self):
+        self._calls = 0
+
+    @property
+    @abstractmethod
+    def x(self):
+        pass
+
+    @property
+    def calls(self):
+        return self._calls
+
+    @staticmethod
+    def onemax(genome):
+        return sum(bool(g) for g in genome)
+
+    def __call__(self, genome):
+        self._calls += 1
+        fitnesses = sorted((AbstractProblem.onemax(genome[s::self.x]) for s in range(self.x)), reverse=True)
+        val = fitnesses[0] - sum(f*(.1 ** (k+1)) for k, f in enumerate(fitnesses[1:]))
+        return val / len(genome) * self.x
+
+def make_problem(a):
+    class Problem(AbstractProblem):
+        @property
+        @abstractmethod
+        def x(self):
+            return a
+
+    return Problem()
+
